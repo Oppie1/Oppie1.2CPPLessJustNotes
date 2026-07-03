929b581c846ce16e56a6fbbbf5a8742f487e0dde
Oppie1 <atindall729@gmail.com>
2026-07-03 16:15:35 -0500

    Add files via upload

---
+++ 1.10Oppie1FncWMltplPrmtrsLeJsNts/1.10Oppie1FncWMltplPrmtrsLeJsNts/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+
+
+//Declare a function prototype addNumbers that has 4 int variables as its parameter
+//addNumbers(parameters here with data type).
+//CODE:
+
+int main() {
+
+	//Make simple cout statement to say what the call returns to screen
+	//CODE:
+
+	//Call the addNumbers(arguments here) as part of a cout statement to the screen 
+	//with 4 integer arguments. You DO NOT need to declare data type again since it has been declared 
+	//in the function.
+	//CODE:
+
+	return 0;
+}
+
+//Create a function definition for int addNumbers().
+//CODE:
+
+	//Here we create a new variable "int answer" which is a local variable that will store the result/sum 
+	//of the expression
+	//CODE:
+
+	//This answer is "returned" to the caller which here is the main() program.
+	//CODE:
+
+//Key takeaway: addNumbers(10,15,28,7) is replaced with the "answer" that is returned in the addNumbers() function outside of main
+//so, addNumbers(10,15,28,7) = answer (return answer)
