e189e1584becbdb48ae93710e414b2a16aff5afd
Oppie1 <atindall729@gmail.com>
2026-07-05 17:20:31 -0500

    Add files via upload

---
+++ 2.36OpiePrntMltiDmnsnlAryLsJsLsNts/1.36OpiePrntMltiDmnsnlAryLsJsLsNts/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+int main() {
+
+
+	//Declare and initialize bertha array with 2 rows and 3 columns. -> 3, 4, 5 and 7, 8, 9
+	//CODE:
+
+	//Output statement that says "This is the bertha multidimensional array" -> Essentially a matrix!
+	//CODE:
+	cout << "This is the bertha multidimensional array -> Essentially a matrix\n\n" << endl;
+
+	//Use for loop and declare an int as "row" initialized to 0 as loop counter, control less than 2 and iterates row by 1.
+	//CODE:
+
+	cout << "\n\n Congrats your back to main() and next step\n" << endl;
+	//Use a nested for loop that is declared column initialized to 0, control less than 3. iterates by 1  
+	//CODE:
+
+	//Output statement within the nested loop that prints values elements at index locations.
+	//CODE:	
+
+	//Create statement that prints a line in between the first and second rows. You're still in the for loop! Must fully execute.
+	//CODE:
+
+
+//Create a line of code that says that say that control has returned to the next statement in main(). 
+//CODE:
+
+}
+//Output:
+//3 4 5
+//7 8 9
---
+++ 2.37Oppie1IntroToPointersJustNotes/2.37Oppie1IntroToPointersJustNotes/main.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+
+
+int main() {
+
+	
+	//Declare a fish variable initialized to 5.
+	//CODE:
+
+	//Out statement that says the memory address of fish is... _ 
+	//CODE:
+
+	//Declare an int pointer variable and store the address of 'fish' in it.
+	//CODE:
+
+	//Output statement that says memory address of fish using the pointer is... _
+	//CODE:
+
+	//Create a cout statement that says outputs should display the same value
+	//CODE:
+	cout<<"Outputs should be identical"<<endl;
+
+
+
+}
