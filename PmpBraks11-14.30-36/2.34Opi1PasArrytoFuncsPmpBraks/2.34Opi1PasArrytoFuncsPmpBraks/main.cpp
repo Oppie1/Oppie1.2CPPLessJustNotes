3e7aaaca2d7e10022e7cd85e45e4de8ddeade9f8
Oppie1 <atindall729@gmail.com>
2026-07-06 17:25:17 -0500

    Add files via upload

---
+++ PmpBraks11-14.30-36/2.34Opi1PasArrytoFuncsPmpBraks/2.34Opi1PasArrytoFuncsPmpBraks/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+
+
+//Declare void function called printArray with the parameters of the array itself (int data type), 
+//as well as an int variable for the size of the array.
+//CODE:
+
+int main() {
+
+	//Declare integer array named adam with 3 elements -> 5, 10, 15
+	//CODE:
+
+	//Declare int array named claudia with 6 elements -> 10, 20, 30, 40, 50, 60.
+	//CODE:
+
+	cout << "The following is the result of the function printArray with parameters claudia and 6" << endl;
+
+	//Call the printArray function with arguments claudia (for array to pull from), and 6 (which corresponds to index location 5.)
+	//CODE:
+
+	cout << "The following is the result of the function printArray with parameters adam and 1" << endl;
+	//Call the printArray function on the adam array and the index location of 1. Which will only print out 2 elements.
+	//CODE:
+
+
+//Define void function definition for the printArray function. 
+//CODE:
+
+//Use for loop that will iterate through the indices. Initialize x to 0, control x less than sizeOfArray variable
+//accounting for each element iterate x by 1. Code block prints location and element value to screen.
+//CODE:
+
+
+//Output statement that will print index number x and then the element itself via theArray variable @ [x]
+//CODE:
+
+}
---
+++ PmpBraks11-14.30-36/2.35Opi1MultidimnsonalAryPmpBraks/2.35Opi1MultidimnsonalAryPmpBraks/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+int main() {
+
+
+
+	//Declare a multi dimensional sally array with 2 rows and 3 columns with numbers 2,3,4 and  8,9,10
+	//CODE:
+
+	cout << "Here we aim find location of elements in a multi-dimensional array named sally. The Array has 6 elements " << endl;
+	cout << "two rows and 3 columns(2x3). { { 2,3,4 }, { 8,9,10 } }" << endl;
+
+	cout << "\nIf it were sally array location @ index 0,1 it would be: " << endl;
+	//Output statement that displays element at INDEX location the array at row 0 and column 1.
+	//CODE:
+
+	cout << "\nIf it were sally location @ index 1,1 it would be: " << endl;
+	//Output statement that displays element at the location of the statement above.
+	//CODE:
+
+	cout << "\nIf it were sally array at index location 1, 2 it would be: " << endl;
+	//Output statement that represents sentence above.
+	//CODE:
+	cout << "\nIf it were sally location @ index 0,0 it would be : " << endl;
+
+	//Create a cout statement that display the location of the statement above.
+	//CODE:
+
+}
