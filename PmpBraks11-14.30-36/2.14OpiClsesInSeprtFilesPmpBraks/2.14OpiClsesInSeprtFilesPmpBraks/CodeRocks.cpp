f7df1f23a409cec2b3cc5360f78b606065f8f18c
Oppie1 <atindall729@gmail.com>
2026-07-06 14:59:08 -0500

    Add files via upload

---
+++ PmpBraks11-14.30-36/2.14OpiClsesInSeprtFilesPmpBraks/2.14OpiClsesInSeprtFilesPmpBraks/CodeRocks.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "CodeRocks.h"
+using namespace std;
+
+
+
+CodeRocks::CodeRocks() {
+
+	cout << "Hellooooooooo" << endl;
+}
---
+++ PmpBraks11-14.30-36/2.14OpiClsesInSeprtFilesPmpBraks/2.14OpiClsesInSeprtFilesPmpBraks/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "CodeRocks.h"
+using namespace std;
+
+
+
+int main() {
+
+	CodeRocks object1;
+
+}
