1edc7d1ee829720dba5d37ccf2f8a191cdc8d6f5
Oppie1 <atindall729@gmail.com>
2026-07-09 21:28:55 -0500

    Refactor main.cpp to improve structure and clarity

--- PmpBraks11-14.30-36/1.11OppieIntroClasNObjsPmpBraks/1.11OppieIntroClasNObjsPmpBraks/main.cpp
+++ PmpBraks11-14.30-36/1.11OppieIntroClasNObjsPmpBraks/1.11OppieIntroClasNObjsPmpBraks/main.cpp
@@ -6,18 +6,6 @@ using namespace std;
 
 //Declare a class named AdamClass
 //CODE:
-class AdamClass {
-
-public:
-
-	string name = "Adam";
-
-	void helloFunction() {
-
-		cout << "Welcome to computer CS my name is: ";
-	}
-
-};
 
 
 //Make a public class access specifier.
@@ -39,15 +27,11 @@ int main() {
 
 	//Create/instantiate object1 and object2 -> generic names. Can name whatever from the AdamClass
 	//CODE:
-	AdamClass object1;
-	AdamClass object2;
 
 	//Use object1 to call the function we created outside of main. Use object2 to 
 	//to call the name variable and then output to screen at same time using cout
 	//to produce a complete sentence.
 	//CODE:
-	object1.helloFunction();
-	cout<<object2.name << endl;
 
 	//Output should be one clean sentence on one line. e.g. "Hello, welcome to intro to computer science my name is Adam. 
 
@@ -57,10 +41,6 @@ int main() {
 	//Use object (here object3) that can hold two pieces of information that can be used. 
 	//Write code that creates object, use SAME object to call helloObject() function and shows name to screen.
 	//CODE:
-	AdamClass object3;
-
-	object3.helloFunction();
-	cout << object3.name << endl;
 
 	return 0;
 
