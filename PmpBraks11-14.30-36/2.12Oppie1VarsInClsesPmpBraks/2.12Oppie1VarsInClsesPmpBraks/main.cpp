757efe43dd074b424c5583989bfca842f5b0310e
Oppie1 <atindall729@gmail.com>
2026-07-09 22:49:22 -0500

    Add files via upload

---
+++ PmpBraks11-14.30-36/2.12Oppie1VarsInClsesPmpBraks/2.12Oppie1VarsInClsesPmpBraks/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+
+//Create a class named codeRocks.
+//CODE:
+
+//Create a public access specifier:
+//CODE:
+
+//Declare void setter function setName() that takes string parameter x and set value x equal to a var that will 
+//store information from main. Make it descriptive like "welcomeStatement".
+//CODE:
+
+
+//Declare a getName() of data type string with no parameters that will return whatever is stored in "welcomeStatement" 
+//variable back to main when called.
+//CODE:
+
+
+//Create a private access specifier.
+//CODE:
+
+//Create the string variable ("welcomeStatement") that will store sentence/string from main.
+//CODE:
+
+
+int main() {
+
+	//Create an object(object1) of the codeRocksClass.
+	//CODE:
+
+	//Call on object1 and string using the setName() function within 
+	//its argument which will store in the welcomeStatement variable.
+	//CODE:
+
+	//Output statement and object1 you created to retrieve the sentence stored in string.
+	//CODE:
+
+}
