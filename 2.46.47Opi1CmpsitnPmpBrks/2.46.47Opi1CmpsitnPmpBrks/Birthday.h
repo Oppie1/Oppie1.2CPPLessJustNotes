466a1cd61d7daa1d486b761991841cc2b73cca4e
Oppie1 <atindall729@gmail.com>
2026-07-11 15:18:42 -0500

    Add files via upload

---
+++ 2.46.47Opi1CmpsitnPmpBrks/2.46.47Opi1CmpsitnPmpBrks/Birthday.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+#include "Birthday.h"
+using namespace std;
+
+
+
+//Declare a class and name it Birthday
+//CODE:
+
+//Declare a public access specifier
+//CODE:
+
+//Declare a constructor Birthday with three parameters int m, int d, int y
+//CODE:
+
+//Declare a void function that prints the date
+//CODE:
+
+//Declare a private access specifier.
+//CODE:
+
+//Declare 3 int member variables (use m_) to help track that they are member variables of the birthday class to represent month, day, year.
+//CODE:
