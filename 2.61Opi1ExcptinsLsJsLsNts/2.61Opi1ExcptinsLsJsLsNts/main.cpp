89514aa078531c18b4bfb02ddd51ebe45f07d376
Oppie1 <atindall729@gmail.com>
2026-07-12 17:00:54 -0500

    Add files via upload

---
+++ 2.61Opi1ExcptinsLsJsLsNts/2.61Opi1ExcptinsLsJsLsNts/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+int main() {
+
+	cout << "Create an error handler/code try/catch that will stop the program without crashing" << endl;
+
+	//Use try keyword to create a codeblock that will have variables and nested if statement with throw.
+	//CODE:
+
+	//Declare and initialize two storage variables that store the ages momsAge and sonsAge.
+	//CODE:
+
+	//Use an if statement to check sonsAge doesn't exceed mom's age. Within the if statement use 
+	//the throw key word with number 99 to catch block or if not true skip if block.
+	//The catch block will also be skipped and so program will go to next step in code.
+	//CODE
+
+	//Use catch keyword with a parameter of int x that will take the integer from throw (here 99) and show
+	//a cout statement to the screen saying Son cant be older than mom and ERROR NUMBER x
+	//CODE:
+
+	//Use the try keyword again.
+	//CODE:
+
+		//Declare two int variables dadsAge and daughtersAge
+		//CODE:
+
+		//Add a cout statement to prompt user for dads age and then a cin statement to retrieve data into int dadsAge variable
+		//CODE:
+
+		//Add a cout statement to prompt user for daughters age and then a cin statement to retrieve data into daughtersAge Variable
+		//CODE:
+
+		//Use if statement to check to see if the daughtersAge is greater than the dads. Then a throw keyword with 92
+		//that will execute (ERROR) if true.
+		//CODE:
+
+		//Add cout statement that says what dads age is with dadsAge variable and same thing for daughter
+
+		//The catch block awaits the throw and places the 92 its int x placeholder effectively setting
+		//the value of x here to 99.
+
+		//Use the catch keyword with an integer parameter variable integer x
+		//CODE:
+
+		//Add cout statement that says dad can not be older than daughter to the screen with "x" error number
+		//CODE:
+
+}
---
+++ 2.62Opi1MorExcptnExmplLsJsLsNts/2.62Opi1MorExcptnExmplLsJsLsNts/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+
+	//Use try Keyword/block
+	//CODE:
+
+		//Declare char variable f and initialize it to 0
+		//CODE:
+
+		//Declare an uninitialized int variable named num1.
+		//CODE
+
+		//Prompt user for first number with cout statement.
+		//CODE:
+		cout << "Please enter a number: " << endl;
+
+		//Input user entry into num1 variable
+		//CODE:
+
+		//Declare an uninitialized int variable named num2.
+		//CODE:
+
+		//Prompt user for the second number.
+		//CODE:
+		cout << "Please enter a second number: " << endl;
+
+		//Input user input and store it in num2
+		//CODE:
+
+		//Use if statement with throw keyword to test if the second user input (num2) is 0
+		//CODE:
+
+		//Use throw with f as what the code block executes if true
+		//CODE:
+
+		//In next part of code block which will execute if the if statement is false (after if code block).
+		//Add a cout statement to the screen the answer is:
+		//CODE:
+
+		//Use a cout statement and an expression num1 divided by num2 to show the answer out to the screen.
+		//CODE:
+
+		//Use the catch all block ... with a cout statement that one cannot divide by 0.
+		//CODE:
+
+}
