0ac3ca341e7215c558498ef1556bea417c08cbe4
Oppie1 <atindall729@gmail.com>
2026-07-18 15:15:28 -0500

    Add files via upload

---
+++ 1.50OpDrvdClsCnstrNDstrcLsJNts/1.54OpDrvdClsCnstrNDstrcLsJNts/Father.h
@@ -0,0 +1,15 @@
+#pragma once
+
+
+
+//Declare a base class named Father
+//CODE:
+
+//Declare a public access specifier
+//CODE:
+
+//Declare a constructor declaration named Father.
+//CODE:
+
+//Declare a destructor named Father
+//CODE:
