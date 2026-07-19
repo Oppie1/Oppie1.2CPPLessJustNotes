47c3af7076597abacb467bec948ce5a66f0a1248
Oppie1 <atindall729@gmail.com>
2026-07-19 15:22:46 -0500

    Add files via upload

---
+++ 1.22Oppie1StockMktSimLsJsNts/1.22Oppie1StockMktSimLsJsNts/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+
+int main() {
+
+	cout << "Welcome to the stock market simulator. You have $1000 with an interest rate of 10% " << endl;
+
+
+	//Declare 3 float variables "total", principle (1000) and rate (10%). 
+	//CODE:
+
+	//Declare an int var to track each day. We will initialize it in for loop.
+
+	//Use a for loop to track 10 days one day at a time that calculates the interest on the principle/total
+	//during each iteration.
+	//CODE:	
+
+		//Given -> This is thr formula to calculate interest on the principle.
+		total = principle * pow(1 + rate, day);
+		
+	//Output a statement that tracks the day we are on that corresponds to the principles value on that day.
+	//CODE:
+
+		//Output statement after for loop has fully executed notifying user that the loop finished successfully and now is day 11.
+	//CODE:
+
+}
