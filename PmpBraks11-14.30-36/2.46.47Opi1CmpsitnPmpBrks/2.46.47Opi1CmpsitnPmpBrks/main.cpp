0ac8a864937acfe6828a23e1a3b480e3a7cca852
Oppie1 <atindall729@gmail.com>
2026-07-23 16:03:52 -0500

    Add files via upload

---
+++ PmpBraks11-14.30-36/2.46.47Opi1CmpsitnPmpBrks/2.46.47Opi1CmpsitnPmpBrks/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "Birthday.h"
+#include "People.h"
+using namespace std;
+
+
+
+int main() {
+
+	//Instantiate object of Birthday class named date of birth obj that takes arguments of Claudia's bday format
+	//CODE:
+
+	//Create a claudiaVilla object of the People class with appropriate arguments.
+	//CODE:
+
+	//Call the the printInfo function on the claudiaVilla object you just created.
+	//CODE:
+
+}
